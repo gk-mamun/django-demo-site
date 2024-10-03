9533875f70b3c1609d522d1830e49f7e4eb1cd5f
Gk Mamun <gkmamun26@gmail.com>
2024-10-03 18:29:14 +0800

    change setting for railway

--- demosite/settings.py
+++ demosite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n5dm)+60mwp$%^y-(50x5kl-bhnsm=@0oj2x!n*ni4&8c=2ayr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") or True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # allow all the railway domain
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1"
+    ]
 
 
 # Application definition
